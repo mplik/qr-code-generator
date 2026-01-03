3890bf3a1f8db220fc6de7c43ba9f6bbf45b2263
mplik <biuro@mplik.pl>
2026-01-03 23:24:52 +0100

    Add print message after QR code generation

--- qr_kod.py
+++ qr_kod.py
@@ -6,3 +6,5 @@ url = "https://mplik.eu"  # Wpisz tu swój adres
 img = qrcode.make(url)
 img.save("qr_code_mplik.png")
 img.show()
+
+print("Plik został zapisany jako 'qr_code_mplik.png' w bieżącym katalogu")

9409666e9187b62d8b008f7e75c8ab0ce126ee72
mplik <biuro@mplik.pl>
2026-01-04 23:41:16 +0100

    Zaktualizowano link wykresu bar dodano kod qr dla wykresu bar

--- qr_kod.py
+++ qr_kod.py
@@ -1,10 +1,10 @@
 import qrcode
 from PIL import Image
 
-url = "https://github.com/mplik/domeny-web/raw/79a7c3a55d9464a4ef671306edb52e07f331d73a/podfoldern/pdf/wykres_kolowy_z_opisem.pdf"  # Wpisz tu swój adres
+url = "https://github.com/mplik/domeny-web/raw/79a7c3a55d9464a4ef671306edb52e07f331d73a/podfoldern/pdf/wykres_z_opisem.pdf"  # Wpisz tu swój adres
 
 img = qrcode.make(url)
-img.save("qr_wykres.png")
+img.save("qr_bar.png")
 img.show()
 
-print("Plik został zapisany jako 'qr_wykres_kolowy.png' w bieżącym katalogu")
+print("Plik został zapisany jako 'qr_bar.png' w bieżącym katalogu")

a01b3d8b7c103cf94315820e0b0ccad8db7cea2a
mplik <biuro@mplik.pl>
2026-01-11 13:45:28 +0100

    Zmiana adresu URL w generatorze kodów dodano plik wygenerowanego kodu qr_meta_horizon.png

--- qr_kod.py
+++ qr_kod.py
@@ -1,10 +1,10 @@
 import qrcode
 from PIL import Image
 
-url = "https://github.com/mplik/domeny-web/raw/78cdb5e5220b65c2d41073aac366f228e49dcced/podfoldern/pdf/mplik_report_watermark_co2.pdf"  # Wpisz tu swój adres
+url = "https://horizon.meta.com/profile/553955491139068/"  # Wpisz tu swój adres
 
 img = qrcode.make(url)
-img.save("qr_full_report.png")
+img.save("qr_meta_horizon.png")
 img.show()
 
-print("Plik został zapisany jako 'qr_full_report.png' w bieżącym katalogu")
+print("Plik został zapisany jako 'qr_meta_horizon.png' w bieżącym katalogu")
